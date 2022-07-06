581ec2313625f89e2ed51fe1691a06611ad05691
hardyethan <108759826+hardyethan@users.noreply.github.com>
2022-07-06 12:40:34 -0400

    Move WPM Calculation and Updating to tick

--- src/main/java/git/hardyethan/wpm/mixin/ChatScreenMixin.java
+++ src/main/java/git/hardyethan/wpm/mixin/ChatScreenMixin.java
@@ -1,6 +1,7 @@
 package git.hardyethan.wpm.mixin;
 
 import git.hardyethan.wpm.render.WPMRenderer;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,39 +17,51 @@ public class ChatScreenMixin {
     private String startingText;
 
 
+    private void reset() {
+        firstUpdate = -1;
+        lastUpdated = -1;
+        startingText = "";
+        WPMRenderer.wpm = -1;
+    }
+
     @Inject(at = @At("HEAD"), method = "onChatFieldUpdate")
     private void onChatFieldUpdate(String newText, CallbackInfo ci) {
         if (newText.length() == 0) {
-            firstUpdate = -1;
-            lastUpdated = -1;
-            startingText = "";
-            WPMRenderer.wpm = -1;
+            reset();
             return;
         }
+
         long newUpdate = System.currentTimeMillis();
         if (lastUpdated == -1) {
             firstUpdate = newUpdate;
             lastUpdated = newUpdate;
             startingText = newText;
-            return;
         }
-        if (newUpdate - lastUpdated < 1000) {
-            return;
-        }
-
-        lastUpdated = System.currentTimeMillis();
-        double charTextLength = (double) (newText.length() - startingText.length()) / 5;
-        WPMRenderer.wpm = (int) (charTextLength / ((int) (lastUpdated / 1000 - firstUpdate / 1000)) * 60);
-
     }
 
     @Inject(at = @At(value = "RETURN"), method = "keyPressed")
     private void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable ci) {
         if (keyCode == GLFW.GLFW_KEY_ESCAPE || keyCode == GLFW.GLFW_KEY_ENTER || keyCode == GLFW.GLFW_KEY_KP_ENTER) {
-            firstUpdate = -1;
-            lastUpdated = -1;
-            startingText = "";
-            WPMRenderer.wpm = -1;
+            reset();
+        }
+    }
+
+    @Inject(at = @At(value = "TAIL"), method = "tick")
+    private void tick(CallbackInfo ci) {
+        if (lastUpdated == -1) {
+            return;
+        }
+
+        long newUpdate = System.currentTimeMillis();
+        if (newUpdate - lastUpdated < 1000) {
+            return;
+        }
+
+        if (MinecraftClient.getInstance().currentScreen instanceof ChatScreen) {
+            String newText = ((ChatScreenInterface) MinecraftClient.getInstance().currentScreen).getChatField().getText();
+            lastUpdated = System.currentTimeMillis();
+            double charTextLength = (double) (newText.length() - startingText.length()) / 5;
+            WPMRenderer.wpm = (int) (charTextLength / ((int) (lastUpdated / 1000 - firstUpdate / 1000)) * 60);
         }
     }
 }
